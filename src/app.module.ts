b609d50d7428da6eb9e3bd1a543b68bc26c5d1d2
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2025-02-28 15:24:35 -0300

    feat(auth): adicionar suporte de autenticação com JWT e Passport

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import { ConfigModule } from '@nestjs/config'
 import { PrismaService } from './prisma/prisma.service'
 
 import { CreateAccountController } from './controllers/create-account.controller'
+import { AuthenticateController } from './controllers/authenticate.controller'
+
+import { AuthModule } from './auth/auth.module'
 
 import { envSchema } from './env'
 
@@ -13,8 +16,9 @@ import { envSchema } from './env'
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
+    AuthModule,
   ],
-  controllers: [CreateAccountController],
+  controllers: [CreateAccountController, AuthenticateController],
   providers: [PrismaService],
 })
 export class AppModule {}
--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -12,6 +12,7 @@ import { hash } from 'bcryptjs'
 import { z } from 'zod'
 
 import { PrismaService } from 'src/prisma/prisma.service'
+
 import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
 
 const createAccountBodySchema = z.object({
